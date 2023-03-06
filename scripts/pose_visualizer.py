ecfb3d490ab23836ae9fc1c3890a6d9f41569ba4
xx-peach <3190102786@zju.edu.cn>
2023-03-06 15:37:12 +0800

    modify pose sequence logic, support circle views and fixed view

--- scripts/pose_visualizer.py
+++ scripts/pose_visualizer.py
@@ -10,31 +10,37 @@ if __name__ == "__main__":
     parser.add_argument('--nb_rendered_human_path', type=str, default='')
     parser.add_argument('--blender_rendered_env_path', type=str, default='')
     parser.add_argument('--n_frames', type=int, default=30)
-    parser.add_argument('--n_views', type=int, default=120)
+    parser.add_argument('--render_view', type=int, default=-1, help='render all test views if -1')
     parser.add_argument('--output_folder', type=str, default='')
     args = parser.parse_args()
 
-    interval = args.n_views // args.n_frames
-
     # read in the background images rendered by blender
     backgrounds = []
+    interval = len(os.listdir(args.blender_rendered_env_path)) // args.n_frames
     for i in range(0, args.n_views, interval):
-        background = np.array(Image.open(os.path.join(args.blender_rendered_env_path, f'{i}.jpg')))
+        view_idx = args.render_view if args.render_view != -1 else i
+        background = np.array(Image.open(os.path.join(args.blender_rendered_env_path, f'{view_idx}.jpg')))
         backgrounds.append(background)
 
     # read in the foreground human images rendered by neuralbody,
     # and generate the human mask at the same time
     foregrounds, masks = [], []
     for i in range(args.n_frames):
-        foreground = np.array(Image.open(os.path.join(args.nb_rendered_human_path, f'rendering', f'{i+1:03d}_{i*4:03d}.png')).convert('RGB'))
+        view_idx = args.render_view if args.render_view != -1 else i
+        foreground = np.array(Image.open(os.path.join(args.nb_rendered_human_path, f'rendering', f'{i+1:03d}_{view_idx:03d}.png')).convert('RGB'))
         mask = foreground.sum(axis=-1) > 5
         foregrounds.append(foreground)
         masks.append(mask)
 
-    # generate the output images
-    output_path = os.path.join(args.output_folder, f'pose_sequence')
+    # define and create the output folder
+    if args.render_view != -1:
+        output_path = os.path.join(args.output_folder, f'pose_sequence/circle')
+    else:
+        output_path = os.path.join(args.output_folder, f'pose_sequence/fixed_view_{args.render_view:02d}')
     os.system(f'rm -rf {output_path}/images')
     os.makedirs(f'{output_path}/images', exist_ok=True)
+
+    # generate the output images
     for i in tqdm.tqdm(range(len(backgrounds))):
         result = (masks[i][..., None] * foregrounds[i] + (1-masks[i])[..., None] * backgrounds[i]).astype(np.uint8)
         imageio.imwrite(os.path.join(output_path, f'images/{i:03d}.jpg'), result)
